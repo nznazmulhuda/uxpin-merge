c30b779e89c6f52f8b0ce8b132897f1867f8e4e0
Nazmul Huda <nznazmulhuda@users.noreply.github.com>
2025-08-30 13:21:34 +0600

    daterangepicker add via jquery, moment, knockout and daterangepicker.js files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import DateRangePicker from "./components/DateRangePicker";
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-lg font-bold mb-4">
+        React DateRangePicker (Raw HTML to JSX)
+      </h1>
+      <DateRangePicker />
+    </div>
+  );
+}

884d0b3b8a4fd97fe4496b9c881803ddb8454c8c
Nazmul Huda <nznazmulhuda@users.noreply.github.com>
2025-08-30 14:48:40 +0600

    added babelrc to load react

--- src/DateRangePicker/presets/0-default.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import DateRangePicker from "../DateRangePicker";
-
-export default <DateRangePicker uxpId="1" />;
---
+++ src/components/DateRangePicker/DateRangePicker.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import DateRangePickerMN from "../../DateRangePicker/DateRangePicker";
+
+export default function DateRangePicker() {
+  return <DateRangePickerMN />;
+}

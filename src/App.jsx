37a216cfaab4dfa4bcf17ab85a4d72d3b6890b84
Nazmul Huda <nznazmulhuda@users.noreply.github.com>
2025-08-30 13:23:12 +0600

    some unuseal file delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import DateRangePicker from "./components/DateRangePicker";
 export default function App() {
   return (
     <div className="p-4">
-      <h1 className="text-lg font-bold mb-4">
-        React DateRangePicker (Raw HTML to JSX)
-      </h1>
       <DateRangePicker />
     </div>
   );
